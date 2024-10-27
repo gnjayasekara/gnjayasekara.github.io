39e3d6cb2b8f097f945cb5d619802112c1b8034a
Nimtharu jayasekara <sagn.jayasekara@gmail.com>
2024-10-28 03:56:52 +0530

    first commit

---
+++ src/data/socials.ts
@@ -0,0 +1,27 @@
+export const socials = [
+  // {
+  //   title: 'Instagram',
+  //   icon: 'mdi:instagram',
+  //   url: 'https://www.instagram.com/jaydanurwin/',
+  // },
+  {
+    title: 'Linkedin',
+    icon: 'mdi:linkedin',
+    url: 'https://www.linkedin.com/in/nimtharu-jayasekara/',
+  },
+  // {
+  //   title: 'YouTube',
+  //   icon: 'mdi:youtube',
+  //   url: 'https://youtube.com/c/LittleSticks',
+  // },
+  {
+    title: 'GitHub',
+    icon: 'mdi:github',
+    url: 'https://github.com/gnjayasekara',
+  },
+  // {
+  //   title: 'Dribbble',
+  //   icon: 'mdi:dribbble',
+  //   url: 'https://dribbble.com/littlesticksdev',  
+  // }
+]
