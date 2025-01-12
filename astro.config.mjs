20ed8885642e8a8a2637955aa52e3e88a327f053
Nimtharu jayasekara <sagn.jayasekara@gmail.com>
2025-01-12 23:12:44 +0530

    Add project assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-import { defineConfig } from 'astro/config';
-import { settings } from './src/data/settings';
-import sitemap from "@astrojs/sitemap";
-
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: settings.site,
-  integrations: [sitemap()],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
-});
+  site: "https://astro-sphere-demo.vercel.app",
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+})
